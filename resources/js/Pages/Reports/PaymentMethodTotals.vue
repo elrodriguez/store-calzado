210db25da3bfa24e2a86643ee39f9ed4ecdddeef
elrodriguez <elrodriguez2423@gmail.com>
2023-03-29 13:39:36 -0500

    desde

--- resources/js/Pages/Reports/PaymentMethodTotals.vue
+++ resources/js/Pages/Reports/PaymentMethodTotals.vue
@@ -77,9 +77,9 @@
                         </div>
                         <div class="col-span-6 p-2 sm:col-span-2 text-right">
                             <Keypad>
-                                <template #botones>
+                                <!-- <template #botones>
                                     <a :href="route('users.create')" class="inline-block px-6 py-2.5 bg-blue-900 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Exportar PDF</a>
-                                </template>
+                                </template> -->
                             </Keypad>
                         </div>
                     </div>
